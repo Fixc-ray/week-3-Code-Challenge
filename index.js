53c3d7d393354d365d6d340fd472234c5272de51
Justin <rayjustin481@gmail.com>
2024-07-14 22:12:01 +0300

    fixed the buy ticket button

--- index.js
+++ index.js
@@ -1,27 +1,50 @@
-const movie = 'http://localhost:3000/films'
+const movie = "http://localhost:3000/films";
 
 fetch(movie)
-.then(res => res.json())
-.then(films => films.forEach(film => createFilm(film)))
+  .then((res) => res.json())
+  .then((films) => films.forEach((film) => createFilm(film)));
 
 function createFilm(film) {
-    const {poster, id, title, description, tickets, showtime, capacity, runtime} = film
-    const wrapper = document.getElementById('movie-list')
-    const divCard = document.createElement('div')
-    divCard.className = 'film'
+  const {
+    poster,
+    id,
+    title,
+    description,
+    tickets_sold,
+    showtime,
+    capacity,
+    runtime,
+  } = film;
+  const wrapper = document.getElementById("movie-list");
+  const divCard = document.createElement("div");
+  const availableTickets = capacity - tickets_sold;
+  const button = document.getElementById("Buy-${id}");
 
+  divCard.className = "film";
 
-    const html = `
+  const html = `
     <img src="${poster}" alt="">
     <p>${id}</p>
     <h2>${title}</h2>
     <p>${description}</p>
     <p>Movie will show at ${showtime}</p>
     <p>Maximum Capacity ${capacity} people</p>
-    <p>Movie will run for ${runtime} minutes</p>`
+    <p>Movie will run for ${runtime} minutes</p>
+    <p>Available Tickets: <span id="tickets-${id}">${availableTickets} </span></p>
+    <button id="Buy-${id}" ${availableTickets === 0 ? "disabled" : ""}>${
+    availableTickets === 0 ? "Sold Out" : "Buy Ticket"
+  }</button>`;
 
-    divCard.innerHTML = html
-    wrapper.appendChild(divCard)
+
+  const list = document.getElementById("side-list")
+  const sidebar = `<h2>${title}</h2>`
+  const sideDiv = document.createElement("div")
+
+  sideDiv, (innerHTML = sidebar);
+  list.appendChild(sideDiv);
+
+  divCard.innerHTML = html;
+  wrapper.appendChild(divCard);
+
+
 }
-
-form.addEventListener("submit", (e) => console.log(e))
